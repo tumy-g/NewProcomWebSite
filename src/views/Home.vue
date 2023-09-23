0bd866f8e5d37197bf8ab7ead5afbfe12d5ad1b1
maizuru1 <maizuru1@kagnoMacBook-Pro.local>
2023-09-23 17:52:50 +0900

    リポジトリ完成andVue2 typescriptの環境を構築完了

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    aaa
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+export default {
+  data(){
+
+  },
+  methods : {
+
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+
+</style>
